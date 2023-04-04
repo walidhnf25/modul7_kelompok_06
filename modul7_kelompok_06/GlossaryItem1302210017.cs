e73462241afd10552887dc84ffe2927cef8eb39c
PC7-IFLAB <>
2023-04-04 17:16:24 +0700

    menambahkan class <rheina.praktikan>

---
+++ modul7_kelompok_06/GlossaryItem1302210017.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul7_kelompok_06
+{
+    internal class GlossaryItem1302210017
+    {
+    }
+}
