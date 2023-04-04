c8e5e35be9d18fa0e48ba0f972beab92d76a6a18
zynexe <zuhayrraffa@gmail.com>
2023-04-04 17:14:34 +0700

    Penambahan kelas TEamMembers

--- modul7_kelompok_06/Program.cs
+++ modul7_kelompok_06/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Newtonsoft.Json;
+using System.Text.Json;
+
+public class TeamMembers1302210068
+{
+    public static void ReadJSON()
+    {
+        string jsonFilePath = @"E:\MOD7\modul7_kelompok_06\modul7_kelompok_06\";
+        string jsonContent = File.ReadAllText(jsonFilePath);
+
+        var members = JsonConverter.DeserializeObject<Dictionary<string, List<Dictionary<string, string>>>>(jsonContent)["member"];
+        Console.WriteLine("Team member list: ");
+
+        foreach (var member in members)
+        {
+            Console.WriteLine($"Anggota {members["nim"]}- {members["firstName"]} {members["lastName"]} {members["age"]} {members["gender"]}");
+        }
+
+        
+    }
+}
+  
+class program
+ {
+    static void Main(string[] args)
+        {
+            TeamMembers1302210068.ReadJSON();
+        }
+ }
+
+
